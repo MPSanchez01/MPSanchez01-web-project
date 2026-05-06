ac909df928d05b33dbaef2e525adad4a56836c94
Marcus Sanchez <Marks011@nmsu.edu>
2026-05-05 18:24:34 -0600

    flock incoming

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,22 @@
 let joinButton = document.getElementById("joinBtn");
 
 joinButton.onclick = function() {
-    let typedName = document.getElementById("userName").value;
+
+    let fName = document.getElementById("firstName").value;
+    let lName = document.getElementById("lastName").value;
+    let email = document.getElementById("userEmail").value;
     let displayArea = document.getElementById("dynamicContent");
 
-    if (typedName !== "") {
-        displayArea.innerHTML = "<h3>Welcome to the Flock, " + typedName + "!</h3>";
-        document.getElementById("userName").value = "";
+    // Check that all fields have text
+    if (fName !== "" && lName !== "" && email !== "") {
+
+        displayArea.innerHTML = "<h3>Success! Welcome to the Flock, " + fName + " " + lName + "!</h3>" +
+                                "<p>A confirmation has been sent to: " + email + "</p>";
+        
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        document.getElementById("userEmail").value = "";
     } else {
-        alert("Please type your name before joining!");
+        alert("Please fill out all fields (First Name, Last Name, and Email) to join!");
     }
 };
