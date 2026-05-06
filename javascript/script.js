02167b5a870fdecaa6582e7d388918ebb1639324
Marcus Sanchez <Marks011@nmsu.edu>
2026-05-05 18:09:50 -0600

    meeeeeeeeeeeeeee

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,12 @@
 let joinButton = document.getElementById("joinBtn");
 
 joinButton.onclick = function() {
-    
     let typedName = document.getElementById("userName").value;
-    
     let displayArea = document.getElementById("dynamicContent");
 
     if (typedName !== "") {
-        
-        let message = "<h3>Welcome to the Flock, " + typedName + "!</h3>";
-        message += "<p>Your passion for birds of prey makes you a vital part of our community.</p>";
-        
-        displayArea.innerHTML = message;
-        
+        displayArea.innerHTML = "<h3>Welcome to the Flock, " + typedName + "!</h3>";
         document.getElementById("userName").value = "";
-        
     } else {
         alert("Please type your name before joining!");
     }
